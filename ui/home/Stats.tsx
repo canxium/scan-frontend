2685bc26e382fb652372c2bda08423654be0c7b0
Neo Krypt <neo@canxium.org>
2025-03-01 15:02:16 +0700

    Remove some emissions

--- ui/home/Stats.tsx
+++ ui/home/Stats.tsx
@@ -128,23 +128,23 @@ const Stats = () => {
         console.log("Failed to get current emission")
       }
       
-      try {
-        const emission24h = BigInt(await emissionAt(latestBlockNum - 14400));
-        let emissionIn24h = currentEmission - emission24h;
-        emissionIn24h = emissionIn24h / BigInt(1e18);
-        set24hEmission(emissionIn24h.toString())
-      } catch (error) {
-        console.log('Failed to get 24h emission')
-      }
+      // try {
+      //   const emission24h = BigInt(await emissionAt(latestBlockNum - 14400));
+      //   let emissionIn24h = currentEmission - emission24h;
+      //   emissionIn24h = emissionIn24h / BigInt(1e18);
+      //   set24hEmission(emissionIn24h.toString())
+      // } catch (error) {
+      //   console.log('Failed to get 24h emission')
+      // }
 
-      try {
-        const emission7d = BigInt(await emissionAt(latestBlockNum - 100800));
-        let emissionIn7d = currentEmission - emission7d;
-        emissionIn7d = emissionIn7d / BigInt(1e18);
-        set7dEmission(emissionIn7d.toString())
-      } catch (error) {
-        console.log('Failed to get 7d emission')
-      }
+      // try {
+      //   const emission7d = BigInt(await emissionAt(latestBlockNum - 100800));
+      //   let emissionIn7d = currentEmission - emission7d;
+      //   emissionIn7d = emissionIn7d / BigInt(1e18);
+      //   set7dEmission(emissionIn7d.toString())
+      // } catch (error) {
+      //   console.log('Failed to get 7d emission')
+      // }
 
       try {
         const emission30d = BigInt(await emissionAt(latestBlockNum - 432000));
@@ -272,18 +272,6 @@ const Stats = () => {
         icon: 'wallet' as const,
         label: 'Wallet addresses',
         value: Number(data.total_addresses).toLocaleString(),
-        isLoading,
-      },
-      {
-        icon: 'token' as const,
-        label: '24h Emissions',
-        value: cau24hEmission + " CAU",
-        isLoading,
-      },
-      {
-        icon: 'token' as const,
-        label: '7d Emissions',
-        value: cau7dEmission + " CAU",
         isLoading,
       },
       {
